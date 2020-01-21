6802882855b57b5cb77e1375dfd3ea0e115d5425
Andrew <andrejj6661@gmail.com>
2020-01-22 02:20:31 +0700

    encoding implemented

--- include/reed_muller.h
+++ include/reed_muller.h
@@ -3,6 +3,8 @@
 #include "utils.h"
 #include "matrix.h"
 
+#include <string>
+
 class ReedMuller {
 	
 	int8_t m_r;
@@ -16,6 +18,26 @@ public:
 	
 	ReedMuller(int8_t r, int8_t m);
 	~ReedMuller();
+	
+	inline std::string encode(const std::string & message) {
+		std::string code = "";
+		
+		Vector mess_vec(message.size());
+		for (size_t i = 0; i < message.size(); ++i) {
+			mess_vec.set(i, message.at(i));
+		}
+		const Matrix & trG = m_G->transpose();
+		for (size_t i = 0; i < (1 << m_m); ++i) {
+			int8_t pseudo_bit = mess_vec.dot(trG.at(i));
+			code += pseudo_bit + '0';
+		}
+		
+		return code;
+	}
+	
+	inline void decode() {
+		
+	}
 	
 private:
 	
